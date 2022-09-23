9f6b70199b9ca3f5fc89182fa533a2ff4ac55b39
Alex Mariano <amarianocello@gmail.com>
2022-09-22 19:43:34 -0700

    Updated Comments

--- C3/calculator.py
+++ C3/calculator.py
@@ -14,23 +14,23 @@ class Calculator:
         integer_two_string = ""
         flag_integer_one_complete = False
         for char in expression:     
-            if char not in allowed_inputs:  # Handling characters, other symbols (*-=/)
+            if char not in allowed_inputs:  # Handling characters, other symbols (Ex. "*-=/asdaff")
                 return "ERROR: Unexpected Input: " + char + ". Input must be alphanumeric and addition only! Ex \"3 + 2\""
             if char == '+':
-                if len(integer_one_string) == 0:
+                if len(integer_one_string) == 0:    # Handles no 1st integer (Ex. "+7328432")
                     return "ERROR: 1st integer not given!"
                 flag_integer_one_complete = True
                 if len(integer_two_string) > 0:    # Handles additional + when 2nd integer is being "read"
                     return "ERROR: Unexpected Input: " + char + ". Calculator can only add TWO integers!"
-                continue
-            if not flag_integer_one_complete: # first integer done, handles multiple ++++++, starts second integer
+                continue    # Handles multiple +
+            if not flag_integer_one_complete: # add to 1st integer until first '+', then add to 2nd integer. 
                 integer_one_string += char
             else:   
                 integer_two_string += char
             index += 1
-        if not flag_integer_one_complete:
+        if not flag_integer_one_complete:   # Handles No addition operator (Ex. "43243")
             return "ERROR: Addition operator not given!"
-        if len(integer_two_string) == 0:    # Handles no 2nd integer, such as "2432+   "
+        if len(integer_two_string) == 0:    # Handles no 2nd integer, (Ex. "2432+   ")
             return "ERROR: 2nd integer not given!"
         
         result = int(integer_one_string) + int(integer_two_string)
