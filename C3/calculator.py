cdaa0ccfd044e67da40087271e35f71d9b86c191
Alex Mariano <amarianocello@gmail.com>
2022-09-22 19:41:16 -0700

    Completed Calculator, added sanitation for input

--- C3/calculator.py
+++ C3/calculator.py
@@ -2,8 +2,38 @@
 class Calculator:
     def eval(self, expression):
         ''' Insert your code to process the string input here '''
-        result = 0
+        # Accepts a STRING
+        # Number, (spaces), addition, (spaces), number
+        # Accept: "2+2", "2 * 2" <-- text error to user, "pineapple" <-- text error to user
+        # Acceptable Inputs: "2 + 2", "2+2", "12 34 + 5 678" (read as 1234 + 5678), "2+++++++++++2" (read as 2 + 2), "2          ++++++++++ 3"
+        # Unacceptable Inputs: "1234", "1234+", "+1234", "fghjekhgjrd"
 
+        allowed_inputs = "0123456789+ "
+        index = 0
+        integer_one_string = ""
+        integer_two_string = ""
+        flag_integer_one_complete = False
+        for char in expression:     
+            if char not in allowed_inputs:  # Handling characters, other symbols (*-=/)
+                return "ERROR: Unexpected Input: " + char + ". Input must be alphanumeric and addition only! Ex \"3 + 2\""
+            if char == '+':
+                if len(integer_one_string) == 0:
+                    return "ERROR: 1st integer not given!"
+                flag_integer_one_complete = True
+                if len(integer_two_string) > 0:    # Handles additional + when 2nd integer is being "read"
+                    return "ERROR: Unexpected Input: " + char + ". Calculator can only add TWO integers!"
+                continue
+            if not flag_integer_one_complete: # first integer done, handles multiple ++++++, starts second integer
+                integer_one_string += char
+            else:   
+                integer_two_string += char
+            index += 1
+        if not flag_integer_one_complete:
+            return "ERROR: Addition operator not given!"
+        if len(integer_two_string) == 0:    # Handles no 2nd integer, such as "2432+   "
+            return "ERROR: 2nd integer not given!"
+        
+        result = int(integer_one_string) + int(integer_two_string)
         return result
 
     def run(self):
