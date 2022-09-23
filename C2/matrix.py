e5d5de3ace592f328d9a1f2f80bd5b1a35e034c6
Alex Mariano <amarianocello@gmail.com>
2022-09-22 17:36:43 -0700

    Completed Matrix

--- C2/matrix.py
+++ C2/matrix.py
@@ -26,7 +26,30 @@ class MatrixOps:
 
     def convolve(self, kernel, matrix):
         ''' Make this function return the result of a 2D convolution '''
-
+        # refer to this index for index orientation:
+        # TL, T, TR
+        # ML, ▇, MR
+        # BL, B, BR
+        neighbor_board_count = np.zeros(matrix.shape)
+        num_rows = matrix.shape[0]
+        num_cols = matrix.shape[1]
+        for r in range(num_rows):
+            for c in range(num_cols):
+                hugging_top = r == 0 # if index is hugging top border --> Disallow T additions
+                hugging_bottom = r == num_rows - 1 #if index is hugging bottom border --> Disallow B additions.
+                if c > 0:   # if not hugging left border, allow L additions
+                    neighbor_board_count[r][c] += matrix[r][c - 1] * kernel[1][0]  #ML
+                    if not hugging_top: neighbor_board_count[r][c] += matrix[r - 1][c - 1] * kernel[0][0]  #TL
+                    if not hugging_bottom: neighbor_board_count[r][c] += matrix[r + 1][c - 1] * kernel[2][0]   #BL
+                if c < num_cols - 1: # if not hugging right border, allow R additions
+                    neighbor_board_count[r][c] += matrix[r][c + 1] * kernel[1][2]  #MR
+                    if not hugging_top: neighbor_board_count[r][c] += matrix[r - 1][c + 1] * kernel[0][2]  #TR
+                    if not hugging_bottom: neighbor_board_count[r][c] += matrix[r + 1][c + 1] * kernel[2][2]   #BR
+                if not hugging_top: neighbor_board_count[r][c] += matrix[r - 1][c] * kernel[0][1]  #T
+                if not hugging_bottom: neighbor_board_count[r][c] += matrix[r + 1][c] * kernel[2][1]  #B
+        for r in range(num_rows):
+            for c in range(num_cols):
+                matrix[r][c] = neighbor_board_count[r][c]
         return matrix
 
     def run(self):
